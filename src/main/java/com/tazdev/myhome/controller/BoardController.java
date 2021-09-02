0119c8a84ab61bae84af2b82067271c9327aacce
taz-dev <tazdev@naver.com>
2021-09-02 18:45:39 +0900

    FEAT: Add form service

--- src/main/java/com/tazdev/myhome/controller/BoardController.java
+++ src/main/java/com/tazdev/myhome/controller/BoardController.java
@@ -5,8 +5,10 @@ import com.tazdev.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -35,7 +37,10 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String formSubmit(@ModelAttribute Board board){
+    public String formSubmit(@Valid Board board, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
         boardRepository.save(board);
         return "redirect:/board/list";
     }
