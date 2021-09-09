b4ae72af8b26956a8465d7e3a85ff0f755603f8a
taz-dev <tazdev@naver.com>
2021-09-10 01:31:27 +0900

    CHORE: @EntityGraph를 통해 N+1문제 해결하기

--- src/main/java/com/tazdev/myhome/controller/UserApiController.java
+++ src/main/java/com/tazdev/myhome/controller/UserApiController.java
@@ -3,6 +3,7 @@ package com.tazdev.myhome.controller;
 import com.tazdev.myhome.model.Board;
 import com.tazdev.myhome.model.User;
 import com.tazdev.myhome.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 class UserApiController {
 
     @Autowired
@@ -18,7 +20,11 @@ class UserApiController {
 
     @GetMapping("/users")
     List<User> all() {
-       return repository.findAll();
+       List<User> users = repository.findAll();
+       log.debug("getBoards().size() 호출전");
+       log.debug("getBoards().size() : {}", users.get(0).getBoards().size());
+       log.debug("getBoards().size() 호출후");
+       return users;
     }
 
     @PostMapping("/users")
--- src/main/java/com/tazdev/myhome/model/User.java
+++ src/main/java/com/tazdev/myhome/model/User.java
@@ -27,7 +27,7 @@ public class User {
     private List<Role> roles = new ArrayList<>();
 
     //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
-    @JsonIgnore
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    //@JsonIgnore
     private List<Board> boards = new ArrayList<>();
 }
