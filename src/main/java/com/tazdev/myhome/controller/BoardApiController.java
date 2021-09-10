fafeb2faa515168c8ad89ad4387eb5e7c4b90a69
taz-dev <tazdev@naver.com>
2021-09-10 16:45:25 +0900

    FEAT: 사용자 권한에 따라 UI 다르게 구성하기, 사용자 권한에 따라 API 제한하기

--- src/main/java/com/tazdev/myhome/controller/BoardApiController.java
+++ src/main/java/com/tazdev/myhome/controller/BoardApiController.java
@@ -3,6 +3,7 @@ package com.tazdev.myhome.controller;
 import com.tazdev.myhome.model.Board;
 import com.tazdev.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
 
@@ -51,6 +52,7 @@ class BoardApiController {
                 });
     }
 
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/boards/{id}")
     void deleteBoard(@PathVariable Long id) {
         repository.deleteById(id);
